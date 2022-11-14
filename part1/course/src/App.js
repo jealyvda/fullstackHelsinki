543fce5eb56c97a3c12b2cedddea82d6f7d61658
jealyvda <jealyvdaker@gmail.com>
2022-11-14 19:38:21 +0100

    Exercise 1A-B

--- part1/course/src/App.js
+++ part1/course/src/App.js
@@ -1,57 +1,61 @@
 const Header = (props) => {
   return (
-    <h1>Hello {props.course}</h1>
+    <h1>Hello {props.props}</h1>
   )
 }
 
-const Part = ({part, exercises}) => {
+
+const Part = (props) => {
   return (<p>
-    {part} {exercises}
+    {props.name} {props.exercises}
   </p>)
 }
 
 const Content = (props) => {
   return (
   <div>
-    <Part part={props.part1} exercises={props.exercises1}/>
-    <Part part={props.part2} exercises={props.exercises2}/>
-    <Part part={props.part3} exercises={props.exercises3}/>
-
+    {
+    props.parts.map((item, index) => {
+      return <Part key={`${index}_${item.name}`} name={item.name} exercises={item.exercises} />
+    })
+  }
   </div>
   )
   }
 
-const Total = (props) => {
-  return (
-    <p>
-      Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}
-    </p>
-  )
-}
+  const Total = (props) => {
+    return (<p>
+      Number of exercises {props.parts.reduce((accumulator, currentValue) => {
+        return accumulator + currentValue.exercises
+      }, 0)}
+      </p>)
+    // return (
+  }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
-  const properties = {
-    course: course, 
-    part1: part1, 
-    part2: part2,
-    part3: part3,
-    exercises1: exercises1, 
-    exercises2: exercises2,
-    exercises3: exercises3
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
   }
+
   return (
     <div>
-      <Header {...properties}/>
-      <Content {...properties}/>
-      <Total {...properties}/>
+      <Header props={course.name}/>
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
     </div>
   )
 }
