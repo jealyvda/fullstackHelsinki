334e694686f339257cec88c8b9cb63009549c7e3
jealyvda <jealyvdaker@gmail.com>
2022-11-23 22:52:45 +0100

    Connect front-end & back-end from Notes application, using Fly.io

--- practice/notes/src/App.js
+++ practice/notes/src/App.js
@@ -22,7 +22,7 @@ const App = (props) => {
   const [notes, setNotes] = useState([]) 
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
-  const [errorMessage, setErrorMessage] = useState('some error happened...')
+  const [errorMessage, setErrorMessage] = useState(null)
 
   useEffect(() => {
     noteService
