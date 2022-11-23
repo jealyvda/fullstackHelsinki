bcbf60cf1dd5c2b77042605e4fe7e569c573376a
jealyvda <jealyvdaker@gmail.com>
2022-11-23 22:52:45 +0100

    Connect front-end & back-end from Notes application, using Fly.io

--- practice/backend/index.js
+++ practice/backend/index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
 app.use(express.json())
+app.use(express.static('build'))
+app.use(cors())
 
 let notes = [
     {
@@ -77,7 +80,7 @@ let notes = [
     response.json(note)
   })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
