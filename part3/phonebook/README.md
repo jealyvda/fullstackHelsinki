ae54044efc36e0fcceacdf321897f262a15fb82e
jealyvda <jealyvdaker@gmail.com>
2022-11-25 20:08:01 +0100

    Launched to internet, created dependency between FE (part2) and BE (part3) Phonebook

---
+++ part3/phonebook/README.md
@@ -0,0 +1,1 @@
+Link to repository via Fly.io: https://fshelsinki-phonebook.fly.dev/
--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,75 +1,77 @@
 const express = require('express')
-let morgan = require('morgan')
 const app = express()
 
+let morgan = require('morgan')
+const cors = require('cors')
+
 app.use(express.json())
+app.use(express.static('build'))
+app.use(cors())
+app.use(morgan("tiny"))
 
-morgan.token('content', (req, res) => {
-    return JSON.stringify(req.body)
-})
+// app.use(morgan(":method :url :status :res[content-length] - :response-time ms :content"))
 
-// app.use(morgan("tiny"))
-app.use(morgan(":method :url :status :res[content-length] - :response-time ms :content"))
-
+// morgan.token('content', (req, res) => {
+//   return JSON.stringify(req.body)
+// })
 
 let persons = [
-        { 
-          "id": 1,
-          "name": "Arto Hellas", 
-          "number": "040-123456"
-        },
-        { 
-          "id": 2,
-          "name": "Ada Lovelace", 
-          "number": "39-44-5323523"
-        },
-        { 
-          "id": 3,
-          "name": "Dan Abramov", 
-          "number": "12-43-234345"
-        },
-        { 
-          "id": 4,
-          "name": "Mary Poppendieck", 
-          "number": "39-23-6423122"
-        }
-    ]
+  { 
+    "id": 1,
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": 2,
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": 3,
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": 4,
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
+]
 
-app.get('/info', (req, res) => {
+app.get('/info', (request, response) => {
     const date = new Date()
-  res.send(`<p>Phonebook has info for ${persons.length} people<br />${date}
+  response.send(`<p>Phonebook has info for ${persons.length} people<br />${date}
   </p>`)
 })
 
-app.get('/api/persons', (req, res) => {
-  res.json(persons)
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
 })
 
-app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
     const person = persons.find(p => p.id === id)
     if (person) {
-        res.json(person)
+        response.json(person)
     } else {
-        res.status(404).end()
+        response.status(404).end()
     }
 })
 
-app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
+app.delete('/api/persons/:id', (request, res) => {
+    const id = Number(request.params.id)
     persons = persons.filter(p => p.id !== id)
 
-    res.status(204).end()
+    response.status(204).end()
 })
 
 const generateRandomId = () => {
     const id = Math.floor(Math.random() * 100000)
-    console.log(id)
     return id
 }
 
-app.post('/api/persons', (req, res) => {
-    const body = req.body
+app.post('/api/persons', (request, response) => {
+    const body = request.body
 
     if (!body.name || !body.number) {
         return res.status(400).json({
@@ -88,10 +90,10 @@ app.post('/api/persons', (req, res) => {
     }
 
     persons = persons.concat(person)
-    res.json(person)
+    response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
