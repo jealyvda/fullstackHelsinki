9d529e2f6308f9e1b3e344871b7c3a0f142c4ca8
jealyvda <jealyvdaker@gmail.com>
2023-01-05 17:31:32 +0100

    Create refactored bloglist 4.1-4.2

---
+++ part4/bloglist/controllers/blogs.js
@@ -0,0 +1,36 @@
+const blogRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogRouter.get('/', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+blogRouter.get('/:id', (request, response, next) => {
+  Blog
+    .findById(request.params.id)
+    .then(blog => {
+      if (blog) {
+        response.json(blog)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
+blogRouter.post('/', (request, response, next) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+    .catch(error => next(error))
+})
+
+module.exports = blogRouter
--- practice/backend/models/note.js
+++ practice/backend/models/note.js
@@ -1,27 +1,14 @@
-require('dotenv').config()
 const mongoose = require('mongoose')
-
-const url = process.env.MONGODB_URI
-
-console.log('connecting to', url)
-
-mongoose.connect(url)
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
 
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
-    minLength: 5,
-    required: true
+    required: true,
+    minlength: 5
   },
   date: {
     type: Date,
-    required: true
+    required: true,
   },
   important: Boolean,
 })
