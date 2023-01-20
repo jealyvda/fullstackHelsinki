5639518f8cc7ee0ec4257f490a09aee4b7d30148
jealyvda <jealyvdaker@gmail.com>
2023-01-20 16:47:29 +0100

    Part 4d: Token authentication for notes app

--- practice/backend/app.js
+++ practice/backend/app.js
@@ -8,6 +8,7 @@ const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 logger.info('connecting to', config.MONGODB_URI)
 
@@ -26,6 +27,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/notes', notesRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
--- practice/backend/controllers/notes.js
+++ practice/backend/controllers/notes.js
@@ -1,6 +1,15 @@
+const jwt = require('jsonwebtoken')
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 const User = require('../models/user')
+
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer')) {
+        return authorization.substring(7)
+    }
+    return null
+}
 
 notesRouter.get('/', async (request, response) => {
     const notes = await Note
@@ -19,7 +28,11 @@ notesRouter.get('/:id', async (request, response) => {
 
 notesRouter.post('/', async (request, response) => {
     const body = request.body
-
+    const token = getTokenFrom(request)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!decodedToken.id) {
+        return response.status(401).json({ error: 'token missing or invalid' })
+    }
     const user = await User.findById(body.userId)
 
     const note = new Note({
@@ -32,6 +45,7 @@ notesRouter.post('/', async (request, response) => {
     const savedNote = await note.save()
     user.notes = user.notes.concat(savedNote._id)
     await user.save()
+
     response.status(201).json(savedNote)
 })
 
@@ -56,3 +70,4 @@ notesRouter.put('/:id', (request, response, next) => {
 })
 
 module.exports = notesRouter
+
