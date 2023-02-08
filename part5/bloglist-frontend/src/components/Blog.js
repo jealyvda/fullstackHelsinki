0093b6ae15e83fcc63886cee4aaf11c090f1c80d
jealyvda <jealyvdaker@gmail.com>
2023-02-08 08:22:14 +0100

    Implementing new front-end integration tests for Note app

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -18,25 +18,39 @@ const Blog = ({ blog, updateLikes, removeBlog, user }) => {
   const toggleVisibility = () => {
     setVisible(!visible)
   }
-
   return(
     <div style={blogStyle}>
       <div style={hideWhenVisible} >
-        {blog.title} <button onClick={toggleVisibility}>view</button>
+        {blog.title} <br />
+        {blog.author}
+        <button onClick={toggleVisibility} id="view">view</button>
       </div>
-      <div style={showWhenVisible} >
+      <div style={showWhenVisible} className="blogContent">
         {blog.title} <button onClick={toggleVisibility}>hide</button><br />
+        {blog.author} <br />
         {blog.url} <br />
         {blog.likes} <button onClick={updateLikes}>like</button><br />
-        {blog.author} <br />
-        {blog.user.username === user.username ? <button onClick={removeBlog}>delete</button> : null}
+        {(blog.user.username === user.username)
+          ? <button onClick={removeBlog}>delete</button>
+          : null}
       </div>
     </div>
   )}
 
 Blog.propTypes = {
+  blog: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired,
+    likes: PropTypes.number,
+  }),
   updateLikes: PropTypes.func.isRequired,
-  removeBlog: PropTypes.func.isRequired
+  removeBlog: PropTypes.func.isRequired,
+  user: PropTypes.shape({
+    token: PropTypes.string,
+    username: PropTypes.string,
+    name: PropTypes.string,
+  }),
 }
 
 export default Blog
---
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import '@testing-library/jest-dom/extend-expect'
+import userEvent from '@testing-library/user-event'
+import { render, screen } from '@testing-library/react'
+import Blog from './Blog'
+
+describe('<Blog />', () => {
+  let container
+
+  const user = {
+    username: 'jane',
+    name: 'Jane Doe',
+    token: '123',
+  }
+
+  const blog = {
+    author: 'Jvda',
+    title: 'Test!',
+    url: 'notimportant',
+    likes: 0,
+    user: user
+  }
+
+  const mockHandlerUpdate = jest.fn()
+  const mockHandlerRemove = jest.fn()
+  beforeEach(() => {
+    container = render(
+      <Blog
+        blog={blog}
+        user={user}
+        removeBlog={mockHandlerRemove}
+        updateLikes={mockHandlerUpdate}
+      >
+      </Blog>
+    ).container
+  })
+
+  test('Always displays author and title', () => {
+    const input = screen.getAllByText('Jvda', { exact: false })
+    const element = screen.getAllByText('Test!', { exact: false })
+  
+    expect(input).toBeDefined()
+    expect(element).toBeDefined()
+  })
+
+  test('at start, children are not displayed', () => {
+    const div = container.querySelector('.blogContent')
+    expect(div).toHaveStyle('display: none')
+  })
+
+  test('after clicking the button, children are displayed', async () => {
+    const user = userEvent.setup()
+    const button = screen.getByText('view')
+    await user.click(button)
+
+    const div = container.querySelector('.blogContent')
+    expect(div).not.toHaveStyle('display:none')
+  })
+})
