45b00149bfb89d61a52891b6de92fbf6d6cf22d2
jealyvda <jealyvdaker@gmail.com>
2023-02-18 16:41:06 +0100

    Building simplified Redux apps for Notes and Counter

---
+++ practice/counter/src/index.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import { createStore } from 'redux'
+
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1
+    case 'DECREMENT':
+      return state - 1
+    case 'ZERO':
+      return 0
+    default: // if none of the above matches, code comes here
+      return state
+  }
+}
+
+const store = createStore(counterReducer)
+
+const App = () => {
+  return (
+    <div>
+      <div>
+        {store.getState()}
+      </div>
+      <button 
+        onClick={e => store.dispatch({ type: 'INCREMENT' })}
+      >
+        plus
+      </button>
+      <button
+        onClick={e => store.dispatch({ type: 'DECREMENT' })}
+      >
+        minus
+      </button>
+      <button 
+        onClick={e => store.dispatch({ type: 'ZERO' })}
+      >
+        zero
+      </button>
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const renderApp = () => {
+  root.render(<App />)
+}
+
+renderApp()
+store.subscribe(renderApp)
