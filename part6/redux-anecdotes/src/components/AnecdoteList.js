c14017844265433c966cab103c897c9c9f4d5b23
jealyvda <jealyvdaker@gmail.com>
2023-02-21 16:55:45 +0100

    6.3-6.8: Updating Anecdote Voting App with Redux store

---
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -0,0 +1,31 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { voteOn } from '../reducers/anecdoteReducer'
+
+const AnecdoteList = () => {
+  const anecdotes = useSelector(state => state)
+  const dispatch = useDispatch()
+
+  const vote = (id) => {
+    dispatch(voteOn(id))
+  }
+
+  return (
+    <div>
+      {anecdotes
+        .sort((a,b) => b.votes - a.votes)
+        .map(anecdote =>
+          <div key={anecdote.id}>
+            <div>
+              {anecdote.content}
+            </div>
+            <div>
+              has {anecdote.votes}
+              <button onClick={() => vote(anecdote.id)}>vote</button>
+            </div>
+          </div>
+        )}
+    </div>
+  )
+}
+
+export default AnecdoteList
