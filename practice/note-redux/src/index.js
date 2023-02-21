404d4a0f032967c139b8df28c47c309ca130b0bb
jealyvda <jealyvdaker@gmail.com>
2023-02-21 17:24:56 +0100

    Implementing radio button filtering on Redux Note App

--- practice/note-redux/src/index.js
+++ practice/note-redux/src/index.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createStore } from 'redux'
+import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
 
 import App from './App'
 import noteReducer from './reducers/noteReducer'
+import filterReducer from './reducers/filterReducer'
+import { createNote } from './reducers/noteReducer'
+import { filterChange } from './reducers/filterReducer'
 
-const store = createStore(noteReducer)
+const reducer = combineReducers({
+  notes: noteReducer,
+  filter: filterReducer
+})
+
+const store = createStore(reducer)
+
+store.subscribe(() => console.log(store.getState()))
+store.dispatch(filterChange('IMPORTANT'))
+store.dispatch(createNote('combineReducers forms one reducer from many simple reducers'))
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
