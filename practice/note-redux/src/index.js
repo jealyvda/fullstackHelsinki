8c52b054d6f2dc0ec44be9d5714545c94c6e3268
jealyvda <jealyvdaker@gmail.com>
2023-02-22 17:07:52 +0100

    Update Note-Redux application with Redux Toolkit for async state management

--- practice/note-redux/src/index.js
+++ practice/note-redux/src/index.js
@@ -1,21 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 
 import App from './App'
-import filterReducer from './reducers/filterReducer'
-
-import noteService from './services/notes'
-import noteReducer, { setNotes } from './reducers/noteReducer'
-
-const store = configureStore({
-  reducer: {
-    notes: noteReducer,
-    filter: filterReducer
-  }
-})
-store.subscribe(() => console.log(store.getState()))
+import store from './store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
--- practice/note-redux/src/reducers/noteReducer.js
+++ practice/note-redux/src/reducers/noteReducer.js
@@ -1,12 +1,10 @@
 import { createSlice } from '@reduxjs/toolkit'
+import noteService from '../services/notes'
 
 const noteSlice = createSlice({
   name: 'notes',
   initialState: [],
   reducers: {
-    createNote(state, action) {
-      state.push(action.payload)
-    },
     toggleImportanceOf(state, action) {
       const id = action.payload
       const noteToChange = state.find(n => n.id === id)
@@ -25,9 +23,20 @@ const noteSlice = createSlice({
   },
 })
 
-const generateId = () =>
-  Number((Math.random() * 1000000).toFixed(0))
+export const { toggleImportanceOf, appendNote, setNotes } = noteSlice.actions
 
-export const { createNote, toggleImportanceOf, appendNote, setNotes } = noteSlice.actions
+export const initializeNotes = () => {
+  return async dispatch => {
+    const notes = await noteService.getAll()
+    dispatch(setNotes(notes))
+  }
+}
+
+export const createNote = content => {
+  return async dispatch => {
+    const newNote = await noteService.createNew(content)
+    dispatch(appendNote(newNote))
+  }
+}
 
 export default noteSlice.reducer
