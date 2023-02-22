a55eb8663cac9e18bc54f53ec5151c9eaf66fa24
jealyvda <jealyvdaker@gmail.com>
2023-02-22 11:45:35 +0100

    6.9: Implementing search filter through Redux store for Anecdotes App

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,11 +1,13 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
+import VisibilityFilter from './components/VisibilityFilter'
 
 const App = () => {
   return (
     <div>
       <h2>Anecdotes</h2>
       <AnecdoteList />
+      <VisibilityFilter />
       <AnecdoteForm />
     </div>
   )
