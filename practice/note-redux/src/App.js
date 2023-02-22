f58fd8abd9f856eabef7bb46d856f737cbc6de94
jealyvda <jealyvdaker@gmail.com>
2023-02-22 17:07:52 +0100

    Update Note-Redux application with Redux Toolkit for async state management

--- practice/note-redux/src/App.js
+++ practice/note-redux/src/App.js
@@ -3,15 +3,12 @@ import { useDispatch } from 'react-redux'
 import NewNote from './components/NewNote'
 import Notes from './components/Notes'
 import VisibilityFilter from './components/VisibilityFilter'
-import noteService from './services/notes'
-import { setNotes } from './reducers/noteReducer'
+import { initializeNotes } from './reducers/noteReducer'
 
 const App = () => {
   const dispatch = useDispatch()
   useEffect(() => {
-    noteService.getAll().then(notes =>
-      dispatch(setNotes(notes))
-    )
+    dispatch(initializeNotes())
   }, []) // eslint-disable-line react-hooks/exhaustive-deps
 
   
