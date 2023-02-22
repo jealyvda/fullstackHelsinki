757f79aaadd155a3b964cc1bd7743ff832e3c548
jealyvda <jealyvdaker@gmail.com>
2023-02-22 16:51:16 +0100

    6.14-6.15: Adding a back-end to the Redux store for Anecdotes App

--- practice/note-redux/src/reducers/noteReducer.js
+++ practice/note-redux/src/reducers/noteReducer.js
@@ -1,29 +1,11 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-const initialState = [
-  {
-    content: 'reducer defines how redux store works',
-    important: true,
-    id: 1,
-  },
-  {
-    content: 'state of store can contain any data',
-    important: false,
-    id: 2,
-  },
-]
-
 const noteSlice = createSlice({
   name: 'notes',
-  initialState,
+  initialState: [],
   reducers: {
     createNote(state, action) {
-      const content = action.payload
-      state.push({
-        content,
-        important: false,
-        id: generateId(),
-      })
+      state.push(action.payload)
     },
     toggleImportanceOf(state, action) {
       const id = action.payload
@@ -32,9 +14,13 @@ const noteSlice = createSlice({
         ...noteToChange,
         important: !noteToChange.important
       }
-
-      console.log(JSON.parse(JSON.stringify(state)))
       return state.map(note => note.id !== id ? note : changedNote)
+    },
+    appendNote(state, action) {
+      state.push(action.payload)
+    },
+    setNotes(state, action) {
+      return action.payload
     }
   },
 })
@@ -42,5 +28,6 @@ const noteSlice = createSlice({
 const generateId = () =>
   Number((Math.random() * 1000000).toFixed(0))
 
-export const { createNote, toggleImportanceOf } = noteSlice.actions
+export const { createNote, toggleImportanceOf, appendNote, setNotes } = noteSlice.actions
+
 export default noteSlice.reducer
