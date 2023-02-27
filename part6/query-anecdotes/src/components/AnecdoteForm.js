5a27adb909508de7ad8d9b209ad7bcfad5954657
jealyvda <jealyvdaker@gmail.com>
2023-02-27 21:20:28 +0100

    6.20-6.22: Implementing anecdote server-state with React Query

--- part6/query-anecdotes/src/components/AnecdoteForm.js
+++ part6/query-anecdotes/src/components/AnecdoteForm.js
@@ -1,10 +1,20 @@
+import { useMutation, useQueryClient } from "react-query"
+import { createAnecdote } from "../requests"
+
 const AnecdoteForm = () => {
+  const queryClient = useQueryClient()
+  const newAnecdoteMutation = useMutation(createAnecdote, {
+    onSuccess: (newAnecdote) => {
+      const anecdotes = queryClient.getQueryData('anecdotes')
+      queryClient.setQueryData('anecdotes', anecdotes.concat(newAnecdote))
+    }
+  })
 
   const onCreate = (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
-    console.log('new anecdote')
+    newAnecdoteMutation.mutate({ content, votes: 0 })
 }
 
   return (
