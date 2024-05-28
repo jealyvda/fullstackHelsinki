5123b65f802841a83cae9fcf56bccd40f017d8d2
jealyvda <jealyvdaker@gmail.com>
2024-05-28 21:44:24 +0200

    12.7: Added get and put routes for ID

--- part12-containers-applications/todo-app/todo-backend/routes/todos.js
+++ part12-containers-applications/todo-app/todo-backend/routes/todos.js
@@ -35,12 +35,13 @@ singleRouter.delete('/', async (req, res) => {
 
 /* GET todo. */
 singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  res.send(req.todo)
 });
 
 /* PUT todo. */
 singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  const todo = await Todo.findOneAndUpdate({ id: req.params.id}, { text: req.body.text, done: req.body.done }, {new: true })
+  res.send(todo)
 });
 
 router.use('/:id', findByIdMiddleware, singleRouter)
